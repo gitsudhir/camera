1bd4d245a56a6ae41936991ccb3ffc26d67523df
gitsudhir <31241120+gitsudhir@users.noreply.github.com>
2019-03-24 00:43:41 +0530

    code of camera

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  userMedia = () => {
+    //  alert('hi there');
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia(
+        { audio: true, video: { width: 880, height: 420 } }
+        , function (stream) {
+          var video = document.getElementById('video');
+          video.srcObject = stream;
+          video.onloadedmetadata = function (e) {
+            video.play();
+          }
+        }
+        , function (err) {
+          console.log('error = ');
+        })
+    }
+  }
+
+ photo =()=>{
+ //alert('photo');
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  canvas.width = 880;
+  canvas.height =420;
+  var video = document.getElementById('video');
+  context.drawImage(video,0,0,880,420);
+  var image = canvas.toDataURL('image/jpeg');
+  // document.getElementById('img').src=image ;
+  console.log('photo');
+  document.querySelector('a').href=image;
+
+
+ }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <button style={{ height: '30px' }} onClick={this.userMedia}>open Camera</button>
+          <button style={{ height: '30px' }} onClick={this.photo}>Take Snapshot</button>
+          <a download='imageFromCamera'>download</a>
+          <video id='video' controls />
+          <canvas id='canvas'> {'no support canvas'} </canvas>
+         {/*
+          <h1>{'your photo/image'}</h1>
+          
+          <img id='img' alt='noImageAvailable'/>
+         */}
+          </header>
       </div>
     );
   }
