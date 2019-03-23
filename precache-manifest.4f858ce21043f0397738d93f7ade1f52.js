18434bac0b3ef5c0972e35a5b0684f801dcc72b3
gitsudhir <31241120+gitsudhir@users.noreply.github.com>
2019-03-24 01:08:34 +0530

    Updates

---
+++ precache-manifest.4f858ce21043f0397738d93f7ade1f52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63bc887b5ddee7ac6cf6",
+    "url": "/camera/static/js/runtime~main.15a0e390.js"
+  },
+  {
+    "revision": "5c7aedda37a2046b7dc2",
+    "url": "/camera/static/js/main.5d907a30.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/camera/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "5c7aedda37a2046b7dc2",
+    "url": "/camera/static/css/main.d6a479cd.chunk.css"
+  },
+  {
+    "revision": "c19f421d2fa564d5014df310efb89e4a",
+    "url": "/camera/index.html"
+  }
+];
